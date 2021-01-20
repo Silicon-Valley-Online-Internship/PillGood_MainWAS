bdbd1565585a2144964d1b950c7db25bdaaff9a7
SeongJin.dev <seongjin.dev@gmail.com>
2021-01-20 23:03:01 +0900

    add UploadAPI Prototype

---
+++ src/main/java/com/example/demo/api/FileUploadController.java
@@ -0,0 +1,38 @@
+package com.example.demo.api;
+
+import com.example.demo.payload.FileUploadResponse;
+import com.example.demo.service.FileUploadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class FileUploadController {
+    private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
+
+    @Autowired
+    private FileUploadService service;
+
+    @GetMapping("/")
+    public String uploadControllerMain() {
+        return "Hello, This is UploadImageAPI Main";
+    }
+
+    @PostMapping("/uploadFile")
+    public FileUploadResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = service.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new FileUploadResponse(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+}
---
+++ src/main/java/com/example/demo/service/FileUploadService.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.FileUploadException;
+import com.example.demo.property.FileUploadProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Service
+public class FileUploadService {
+    private final Path fileLocation;
+
+    @Autowired
+    public FileUploadService(FileUploadProperties prop) {
+        this.fileLocation = Paths.get(prop.getUploadDir())
+                .toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileLocation);
+        } catch (Exception e) {
+            throw new FileUploadException("파일을 업로드할 디렉토리를 생성하지 못했습니다.", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+
+        try {
+            // 파일명에 부적합 문자가 있는지 확인한다.
+            if(fileName.contains(".."))
+                throw new FileUploadException("파일명에 부적합 문자가 포함되어 있습니다. " + fileName);
+
+            Path targetLocation = this.fileLocation.resolve(fileName);
+
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (Exception e) {
+            throw new FileUploadException("["+fileName+"] 파일 업로드에 실패하였습니다. 다시 시도하십시오.",e);
+        }
+    }
+}
